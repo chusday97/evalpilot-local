7e62ffa0da41f7f019c75c9a9309dd72b24fc927
chusday97 <chusday97@gmail.com>
2026-08-15 23:25:07 +0800

    feat: make Projects the dashboard landing context

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.js';
+import { applyProjectFirstLanding } from './project-first-route.js';
 import './styles.css';
 import './modal.css';
+
+applyProjectFirstLanding();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
